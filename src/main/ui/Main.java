af83e4d84321788c288132ad5608b71f8d686b11
Esther Hsueh <snow113@students.cs.ubc.ca>
2021-08-09 22:47:16 +0800

    add frame to main

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 public class Main {
     public static void main(String[] args) {
         try {
+            new CaseFrame();
             new CaseTracker();
         } catch (FileNotFoundException e) {
             System.out.println("Unable to run application: file not found");
